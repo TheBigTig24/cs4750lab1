293a554b4e78bc65a27cf601639f70e0dd3c815d
Caleb C <kaichun24@gmail.com>
2025-09-03 15:13:12 -0700

    klondike prep

---
+++ lib/klondike.dart
@@ -0,0 +1,68 @@
+import 'package:cs_4750_lab1/components/foundation.dart';
+import 'package:cs_4750_lab1/components/pile.dart';
+import 'package:cs_4750_lab1/components/stock.dart';
+import 'package:cs_4750_lab1/components/waste.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
+
+class Klondike extends FlameGame {
+
+	// declare global constants
+	static const double cardWidth = 1000.0;
+	static const double cardHeight = 1400.0;
+	static const double cardGap = 175.0;
+	static const double cardRadius = 100.0;
+	static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
+
+	@override
+	Future<void> onLoad() async {
+		// load sprite sheet into image cache
+		await Flame.images.load('klondike-sprites.png');
+
+		// create components
+		final stock = Stock()
+			..size = cardSize
+			..position = Vector2(cardGap, cardGap);
+
+		final waste = Waste()
+			..size = cardSize
+			..position = Vector2(cardWidth + 2 * cardGap, cardGap);
+
+		final foundations = List.generate(
+				2,
+				(i) => Foundation()
+					..size = cardSize
+					..position = Vector2((i + 3) * (cardWidth + cardGap) + cardGap, cardGap),
+			);
+
+		final piles = List.generate(
+				7,
+				(i) => Pile()
+					..size = cardSize
+					..position = Vector2(
+						cardGap + i * (cardWidth + cardGap),
+						cardHeight + 2 * cardGap
+					),
+			);
+
+		// add components to world
+		world.add(stock);
+		world.add(waste);
+		world.addAll(foundations);
+		world.addAll(piles);
+
+		// set player viewfinder
+		camera.viewfinder.visibleGameSize = Vector2(cardWidth * 7 + cardGap * 8, 4 * cardHeight + 3 * cardGap);
+		camera.viewfinder.position = Vector2(cardWidth * 3.5 + cardGap * 4, 0);
+		camera.viewfinder.anchor = Anchor.topCenter;
+	}
+
+	Sprite klondikeSprite(double x, double y, double width, double height) {
+		return Sprite(
+			Flame.images.fromCache('klondike-sprites.png'),
+			srcPosition: Vector2(x, y),
+			srcSize: Vector2(width, height),
+		);
+	}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:cs_4750_lab1/klondike.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  final game = FlameGame();
+  final game = Klondike();
   runApp(GameWidget(game: game));
 }
